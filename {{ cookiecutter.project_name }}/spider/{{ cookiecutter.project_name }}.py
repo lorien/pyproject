1b579312314ad472bf300fdbd5572e33762714f3
Gregory Petukhov <lorien@lorien.name>
2019-02-03 00:33:13 +0300

    Fix

--- {{ cookiecutter.project_name }}/spider/{{ cookiecutter.project_name }}.py
+++ {{ cookiecutter.project_name }}/spider/{{ cookiecutter.project_name }}.py
@@ -1,9 +1,9 @@
 from pprint import pprint
 from urllib.parse import quote_plus, urlsplit, urljoin
+from datetime import datetime
 
 from grab.spider import Spider, Task
 from grab import Grab
-from grab.spider.decorators import integrity
 from weblib.error import DataNotValid
 
 from project.database import db
